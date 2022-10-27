d57b91a040cfbddcdff7f0ef0b73e4f5e895bfe0
Santiago Carranza <santiagocarranzazinny@gmail.com>
2022-10-27 13:37:46 -0300

    import en views

--- conf/confApp/views.py
+++ conf/confApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from .models import Equipos
 
 def home_screen_view(request):
     equipos = Equipos.objects.all
 
-    return render(request, "base.html", {'clientess':cliente})
+    return render(request, "base.html", {'equipo':equipos})
